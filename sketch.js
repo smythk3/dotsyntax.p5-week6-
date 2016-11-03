cd3030759bf2a1a79e9de64d3976d24c6ec8aaf9
smythk3 <smythk3@tcd.ie>
2016-11-03 15:40:10 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var circle = { 
+  x : 100,
+  y : 100,
+  diam: 100,
+}
+
+function setup() {
+  createCanvas(500,800);
+  
+  
+}
+
+function draw() {
+  ellipse(circle.x, circle.y, circle.diam);
+  
+}
